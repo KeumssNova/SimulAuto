8a70c95b96d96d2f53c639ec073004429e76f84e
salym da costa <salymdc@gmail.com>
2025-07-27 13:45:59 +0200

    fix server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({path: "/etc/secrets/.env"});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
