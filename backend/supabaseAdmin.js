773e1b2fe879b609db073cc15377e2ebbb9b8f22
salym da costa <salymdc@gmail.com>
2025-08-02 20:43:22 +0200

    adding limitation on plans

--- backend/supabaseAdmin.js
+++ backend/supabaseAdmin.js
@@ -5,3 +5,5 @@ const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseServiceKey = process.env.SUPABASE_KEY;
 
 export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey);
+
+export default supabaseAdmin;
